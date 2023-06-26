d2f5128000f9ec4ac1e3f53313f14da15269e899
孙焱 <854591328@qq.com>
2023-06-26 13:48:40 +0800

    修改粒子

--- src/components/canvas/partcleView/ParticleView.jsx
+++ src/components/canvas/partcleView/ParticleView.jsx
@@ -7,11 +7,11 @@ import FBOParticleMesh from "./child/fboParticleMesh/FBOParticleMesh";
 
 function ParticleView() {
   return (
-    <group>
+    <group scale={0.8} rotation={[Math.PI/2,0,0]} position={[0,0,0]}>
       <group scale={[1, 0.01, 1]}>
         <CustomGeometryParticles count={2000} shape="sphere" />
       </group>
-      <group scale={[2, 2, 2]}>
+      <group scale={[1, 1, 1]}>
         <FBOParticleMesh />
       </group>
     </group>
